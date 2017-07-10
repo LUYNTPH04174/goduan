dbdf92a2eeda8f209930b79b15be97684943cbff
LuyNT <nguyenthanhluynd@gmail.com>
2017-07-11 00:47:14 +0700

    Khoi tao du an

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+ 	"goduan/route"
+ 	
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/login",route.GetLoginAnUser)
+	rgroup:=r.Group("/app")
+	{
+		rgroup.POST("/post",route.InsertAnUserRouter)
+	}
+	
+	r.Run(":8080")
+}
---
+++ model/user.go
@@ -0,0 +1,31 @@
+package model
+import (
+	// "encoding/json"
+	"gopkg.in/mgo.v2/bson"
+)
+type User struct {
+	Id     bson.ObjectId `json:"id" bson:"_id"`
+	Sdt        string `form:"sdt" json:"sdt" bson:"sdt"`
+	Password   string `form:"password" json:"password" bson:"password"`
+	Facebookid string `form:"facebookid" json:"facebookid" bson:"facebookid"`
+}
+
+func (user *User) SetValue(a, b, c string) {
+	user.Id=bson.NewObjectId()
+	user.Sdt = a
+	user.Password = b
+	user.Facebookid = c
+}
+
+func (user *User) GetSDT() string {
+	return user.Sdt 
+}
+
+func (user *User) GetPassWord() string {
+	return user.Password 
+}
+
+func (user *User) GetFacebook() string {
+	return user.Facebookid 
+}
+
